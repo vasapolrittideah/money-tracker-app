30bacc5d2f73e3bf8ced55633e4c56fd29c5f8be
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-08-29 10:14:59 +0700

    feat(core): implement base module structure and service locator

---
+++ modules/core/lib/core.dart
@@ -0,0 +1,3 @@
+export 'src/config/app_config.dart';
+export 'src/module/base_module.dart';
+export 'src/core_module.dart';
---
+++ modules/core/lib/src/core_module.dart
@@ -0,0 +1,6 @@
+import 'package:core/core.dart';
+
+class CoreModule extends BaseModule {
+  @override
+  void setupDependencies() {}
+}
