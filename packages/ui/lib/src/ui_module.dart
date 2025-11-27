9c2b17ba2eea732ac3ad79728e12a95a2761106e
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-11-27 11:09:18 +0700

    feat(ui): initialize standalone UI package for isolating UI-related code

---
+++ packages/ui/lib/src/ui_module.dart
@@ -0,0 +1,11 @@
+import 'package:shared/shared.dart';
+
+class UiModule extends BaseModule {
+  @override
+  String get name => 'ui';
+
+  @override
+  void initialize() {
+    // Initialize shared module dependencies and configurations here.
+  }
+}
