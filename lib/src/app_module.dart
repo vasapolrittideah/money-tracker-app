3db1f44dc0b0bb739b955e28914f926000f214bb
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2025-11-28 10:09:57 +0700

    feat(core): include UI and auth modules during application intialization

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,9 +1,11 @@
+import 'package:auth/auth.dart';
 import 'package:shared/shared.dart';
+import 'package:ui/ui.dart';
 
 class AppModule {
   AppModule._();
 
-  static final List<BaseModule> _packages = [SharedModule()];
+  static final List<BaseModule> _packages = [SharedModule(), UiModule(), AuthModule()];
 
   static void initialize() {
     for (var module in _packages) {
