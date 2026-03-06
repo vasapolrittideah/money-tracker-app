ba2deb847ad647b2879a3fb9b77e1a0a27f95fc4
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-06 11:09:59 +0700

    fix(core): resolve incorrect rotation pivot for RotationTransition in AppLoadingIndicator

--- lib/src/core/widgets/feedback/loading_indicator.dart
+++ lib/src/core/widgets/feedback/loading_indicator.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:money_tracker/src/core/theme/theme_provider.dart';
 import 'package:remixicon/remixicon.dart';
 
@@ -10,9 +11,10 @@ class AppLoadingIndicator extends HookWidget {
   Widget build(BuildContext context) {
     final controller = useAnimationController(duration: const Duration(seconds: 1))..repeat();
 
-    return Center(
-      child: RotationTransition(
-        turns: controller,
+    return RotationTransition(
+      turns: controller,
+      child: SizedBox.square(
+        dimension: context.dimensions.dim6.r,
         child: Icon(RemixIcons.loader_2_fill, color: context.colors.textSoft400),
       ),
     );
