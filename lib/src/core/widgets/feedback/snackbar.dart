b9f3da72b7439f1f2ef88e620429b7fdd9f5706a
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-07 08:45:21 +0700

    feat(core): add AppSnackbar widget for customizable snack bar notifications

---
+++ lib/src/core/widgets/feedback/snackbar.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:money_tracker/src/core/theme/theme_provider.dart';
+import 'package:remixicon/remixicon.dart';
+
+enum SnackbarType { success, error, info }
+
+class AppSnackbar {
+  static ScaffoldFeatureController<SnackBar, SnackBarClosedReason> show(
+    BuildContext context,
+    String message, {
+    SnackbarType type = SnackbarType.info,
+  }) {
+    final Color effectiveBackgroundColor = switch (type) {
+      SnackbarType.success => context.colors.successLight,
+      SnackbarType.error => context.colors.errorLight,
+      SnackbarType.info => context.colors.infoLight,
+    };
+
+    final Color effectiveTextColor = switch (type) {
+      SnackbarType.success => context.colors.successDark,
+      SnackbarType.error => context.colors.errorDark,
+      SnackbarType.info => context.colors.infoDark,
+    };
+
+    final effectiveIcon = switch (type) {
+      SnackbarType.success => RemixIcons.check_line,
+      SnackbarType.error => RemixIcons.error_warning_line,
+      SnackbarType.info => RemixIcons.information_line,
+    };
+
+    final Widget snackbarContent = Padding(
+      padding: EdgeInsets.only(top: context.dimensions.dim1.h, bottom: context.dimensions.dim3.h),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(effectiveIcon, color: effectiveTextColor, size: context.dimensions.dim5.r),
+          SizedBox(width: context.dimensions.dim2.w),
+          Expanded(
+            child: Text(message, style: context.typography.textBase.copyWith(color: effectiveTextColor)),
+          ),
+        ],
+      ),
+    );
+
+    final messenger = ScaffoldMessenger.of(context);
+    messenger.removeCurrentSnackBar();
+    return messenger.showSnackBar(SnackBar(backgroundColor: effectiveBackgroundColor, content: snackbarContent));
+  }
+}
