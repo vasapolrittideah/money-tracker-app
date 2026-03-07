5f916b8155beb9cbf89824488d217124e3a876ca
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-07 08:55:13 +0700

    feat(splash): add delay 2 seconds for navigation to SelectLoginMethodScreen

--- lib/src/features/splash/views/splash_screen.dart
+++ lib/src/features/splash/views/splash_screen.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:money_tracker/src/app_router.dart';
 import 'package:money_tracker/src/core/gen/assets.gen.dart';
 import 'package:money_tracker/src/core/theme/theme_provider.dart';
 import 'package:money_tracker/src/core/widgets/feedback/loading_indicator.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends HookWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    useEffect(() {
+      Future.delayed(const Duration(seconds: 2), () {
+        if (context.mounted) context.go(AppRouter.selectLoginMethod);
+      });
+
+      return null;
+    }, const []);
+
     return Scaffold(
       backgroundColor: context.colors.bgWhite0,
       body: SafeArea(
         child: Stack(
           children: [
             Center(
-              child: Assets.images.logo.svg(width: context.dimensions.dim24, color: context.colors.primaryBase),
+              child: Assets.images.logo.svg(width: context.dimensions.dim24.w, color: context.colors.primaryBase),
             ),
             Positioned(bottom: context.dimensions.dim10, left: 0, right: 0, child: AppLoadingIndicator()),
           ],
