bdd6b5b85c3a7c163ab2b03396de8c4e6e460d60
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-10 11:05:45 +0700

    feat(auth): integrate RegisterForm with AuthViewModel for registration

--- lib/src/features/auth/views/widgets/register_form.dart
+++ lib/src/features/auth/views/widgets/register_form.dart
@@ -3,9 +3,13 @@ import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:money_tracker/src/core/theme/theme_provider.dart';
 import 'package:money_tracker/src/core/widgets/buttons/button.dart';
+import 'package:money_tracker/src/core/widgets/feedback/snackbar.dart';
 import 'package:money_tracker/src/core/widgets/inputs/text_input.dart';
+import 'package:money_tracker/src/features/auth/models/register/register.dart';
+import 'package:money_tracker/src/features/auth/viewmodels/auth/auth_viewmodel.dart';
 import 'package:remixicon/remixicon.dart';
 
 final _formKey = GlobalKey<FormBuilderState>();
@@ -13,15 +17,23 @@ final _emailTextInputKey = 'email';
 final _passwordTextInputKey = 'password';
 final _nameTextInputKey = 'name';
 
-class RegisterForm extends HookWidget {
+class RegisterForm extends HookConsumerWidget {
   const RegisterForm({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final emailFocusNode = useFocusNode();
     final passwordFocusNode = useFocusNode();
     final nameFocusNode = useFocusNode();
     final passwordHasError = useState(false);
+
+    ref.listen(authViewModelProvider, (previous, next) {
+      next.whenOrNull(
+        error: (error) {
+          AppSnackbar.show(context, error.message, type: SnackbarType.error);
+        },
+      );
+    });
 
     return FormBuilder(
       key: _formKey,
@@ -102,7 +114,11 @@ class RegisterForm extends HookWidget {
             onPressed: () {
               if (_formKey.currentState?.saveAndValidate() ?? false) {
                 final formData = _formKey.currentState?.value;
-                print('Form Data: $formData');
+                final request = Register(
+                  email: formData?[_emailTextInputKey] as String,
+                  password: formData?[_passwordTextInputKey] as String,
+                );
+                ref.read(authViewModelProvider.notifier).register(request);
               }
             },
           ),
