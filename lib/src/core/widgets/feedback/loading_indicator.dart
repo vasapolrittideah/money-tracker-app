0ab2cca827d5630e8921e0215ad538e8ff3ef584
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-12 00:39:47 +0700

    feat(core): enhance AppLoadingIndicator to accept custom color

--- lib/src/core/widgets/feedback/loading_indicator.dart
+++ lib/src/core/widgets/feedback/loading_indicator.dart
@@ -5,17 +5,21 @@ import 'package:money_tracker/src/core/theme/theme_provider.dart';
 import 'package:remixicon/remixicon.dart';
 
 class AppLoadingIndicator extends HookWidget {
-  const AppLoadingIndicator({super.key});
+  const AppLoadingIndicator({super.key, this.color});
+
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     final controller = useAnimationController(duration: const Duration(seconds: 1))..repeat();
 
+    final effectiveColor = color ?? context.colors.textSoft400;
+
     return RotationTransition(
       turns: controller,
       child: SizedBox.square(
         dimension: context.dimensions.dim6.r,
-        child: Icon(RemixIcons.loader_2_fill, color: context.colors.textSoft400),
+        child: Icon(RemixIcons.loader_2_fill, color: effectiveColor),
       ),
     );
   }
