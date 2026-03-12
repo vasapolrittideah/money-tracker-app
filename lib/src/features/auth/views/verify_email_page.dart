b4f13474da693d5dd481b213bd617ad95ed9c2c2
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-12 14:55:24 +0700

    feat(auth): add countdown functionality for resend email and alert dialog to verify email page

--- lib/src/features/auth/views/verify_email_page.dart
+++ lib/src/features/auth/views/verify_email_page.dart
@@ -1,89 +1,143 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:money_tracker/src/core/theme/theme_provider.dart';
 import 'package:money_tracker/src/core/widgets/buttons/button.dart';
+import 'package:money_tracker/src/core/widgets/feedback/alert_dialog.dart';
+import 'package:money_tracker/src/core/widgets/feedback/loading_indicator.dart';
 import 'package:money_tracker/src/core/widgets/layout/header.dart';
+import 'package:money_tracker/src/core/widgets/overlays/dialog.dart';
+import 'package:money_tracker/src/features/auth/viewmodels/account/account_notifier.dart';
+import 'package:money_tracker/src/features/auth/viewmodels/email_verification/email_verification_notifier.dart';
 import 'package:money_tracker/src/features/auth/views/widgets/verify_email_form.dart';
 import 'package:remixicon/remixicon.dart';
 
-class VerifyEmailPage extends StatelessWidget {
+const resendCooldown = 30;
+
+class VerifyEmailPage extends HookConsumerWidget {
   const VerifyEmailPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final accountState = ref.watch(accountProvider);
+    final countdown = useState(resendCooldown);
+
+    useEffect(() {
+      countdown.value = resendCooldown;
+      final timer = Stream.periodic(
+        const Duration(seconds: 1),
+        (i) => resendCooldown - i - 1,
+      ).take(resendCooldown).listen((value) => countdown.value = value);
+      return timer.cancel;
+    }, const []);
+
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        accountState.whenOrNull(
+          error: (error) {
+            AppDialog.show(
+              context: context,
+              child: AppAlertDialog(
+                title: 'เกิดข้อผิดพลาด',
+                message: error.message,
+                buttonText: 'ลองใหม่อีกครั้ง',
+                type: AlertDialogType.error,
+                onButtonPressed: () {
+                  ref.read(accountProvider.notifier).loadAccount();
+                },
+              ),
+            );
+          },
+        );
+      });
+
+      return null;
+    }, [accountState]);
+
     return Scaffold(
       backgroundColor: context.colors.bgWhite0,
       appBar: AppHeader(showLogo: true),
       body: SafeArea(
         child: Column(
-          mainAxisSize: MainAxisSize.max,
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            SingleChildScrollView(
-              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
-              padding: EdgeInsets.fromLTRB(
-                context.dimensions.dim4.w,
-                context.dimensions.dim8.h,
-                context.dimensions.dim4.w,
-                0,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text(
-                    'ยืนยันอีเมล',
-                    style: context.typography.textDisplay.copyWith(
-                      color: context.colors.textStrong950,
-                      fontWeight: FontWeight.bold,
+            accountState.maybeMap(
+              loaded: (value) => SingleChildScrollView(
+                keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+                padding: EdgeInsets.fromLTRB(
+                  context.dimensions.dim4.w,
+                  context.dimensions.dim8.h,
+                  context.dimensions.dim4.w,
+                  0,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'ยืนยันอีเมล',
+                      style: context.typography.textDisplay.copyWith(
+                        color: context.colors.textStrong950,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: context.dimensions.dim4.h),
-                  Text(
-                    'กรอกรหัสยืนยันที่ถูกส่งไปยังอีเมลของคุณ',
-                    style: context.typography.textBase.copyWith(color: context.colors.textSub600),
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: context.dimensions.dim1.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'test@example.com',
-                        style: context.typography.textBase.copyWith(
-                          color: context.colors.textStrong950,
-                          fontWeight: FontWeight.w500,
+                    SizedBox(height: context.dimensions.dim4.h),
+                    Text(
+                      'กรอกรหัสยืนยันที่ถูกส่งไปยังอีเมลของคุณ',
+                      style: context.typography.textBase.copyWith(color: context.colors.textSub600),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: context.dimensions.dim1.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          value.account.email,
+                          style: context.typography.textBase.copyWith(
+                            color: context.colors.textStrong950,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          textAlign: TextAlign.center,
                         ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(width: context.dimensions.dim1.w),
-                      Icon(RemixIcons.pencil_line),
-                    ],
-                  ),
-                  SizedBox(height: context.dimensions.dim10.h),
-                  VerifyEmailForm(),
-                  SizedBox(height: context.dimensions.dim6.h),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'ไม่ได้รับรหัสยืนยัน? ',
-                        style: context.typography.textBase.copyWith(color: context.colors.textSub600),
-                      ),
-                      SizedBox(height: context.dimensions.dim1.h),
-                      Align(
-                        alignment: Alignment.center,
-                        child: AppButton(
-                          text: 'ส่งรหัสอีกครั้งใน 00:30',
-                          variant: ButtonVariant.text,
-                          fullWidth: false,
-                          disabled: true,
-                          onPressed: () {},
+                        SizedBox(width: context.dimensions.dim1.w),
+                        Icon(RemixIcons.pencil_line),
+                      ],
+                    ),
+                    SizedBox(height: context.dimensions.dim10.h),
+                    VerifyEmailForm(),
+                    SizedBox(height: context.dimensions.dim6.h),
+                    Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'ไม่ได้รับรหัสยืนยัน? ',
+                          style: context.typography.textBase.copyWith(color: context.colors.textSub600),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        SizedBox(height: context.dimensions.dim1.h),
+                        Align(
+                          alignment: Alignment.center,
+                          child: AppButton(
+                            text: countdown.value > 0
+                                ? 'ส่งรหัสอีกครั้งใน 00:${countdown.value.toString().padLeft(2, '0')}'
+                                : 'ส่งรหัสอีกครั้ง',
+                            variant: ButtonVariant.text,
+                            fullWidth: false,
+                            disabled: countdown.value > 0,
+                            onPressed: () {
+                              countdown.value = resendCooldown;
+                              Stream.periodic(
+                                const Duration(seconds: 1),
+                                (i) => resendCooldown - i - 1,
+                              ).take(resendCooldown).listen((v) => countdown.value = v);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
+              orElse: () => Expanded(child: Center(child: AppLoadingIndicator())),
             ),
           ],
         ),
