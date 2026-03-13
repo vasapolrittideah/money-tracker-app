d0e9f40aed9437ca2ceccc74e93b867343c264a4
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-14 01:54:07 +0700

    feat(core): refactor ApiClient to include session management and token handling

--- lib/src/core/network/api_client.dart
+++ lib/src/core/network/api_client.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:money_tracker/src/core/config/app_config.dart';
+import 'package:money_tracker/src/core/session/session.dart';
+import 'package:money_tracker/src/core/session/session_notifier.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:ua_client_hints/ua_client_hints.dart';
@@ -10,11 +12,11 @@ part 'api_client.g.dart';
 @Riverpod(keepAlive: true)
 ApiClient apiClient(Ref ref) {
   final appConfig = ref.watch(appConfigProvider);
-  return ApiClient(baseUrl: appConfig.apiUrl, enableLogger: appConfig.isDevelopment);
+  return ApiClient(ref, baseUrl: appConfig.apiUrl, enableLogger: appConfig.isDevelopment);
 }
 
 class ApiClient {
-  ApiClient({required String baseUrl, bool enableLogger = false}) {
+  ApiClient(Ref ref, {required String baseUrl, bool enableLogger = false}) {
     dio = Dio(
       BaseOptions(
         baseUrl: baseUrl,
@@ -29,6 +31,10 @@ class ApiClient {
     dio.interceptors.add(
       InterceptorsWrapper(
         onRequest: (options, handler) async {
+          final session = await ref.read(sessionProvider.future);
+          if (session != null) {
+            options.headers.addAll(_tokenHeader(session));
+          }
           options.headers.addAll(await userAgentClientHintsHeader());
           return handler.next(options);
         },
@@ -50,4 +56,8 @@ class ApiClient {
   }
 
   late Dio dio;
+
+  Map<String, String> _tokenHeader(Session session) {
+    return {'Authorization': 'Bearer ${session.accessToken}'};
+  }
 }
