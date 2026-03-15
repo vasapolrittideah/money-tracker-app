4480b86e6588f0d4e615e8856daff66bf5839002
Vasapol Rittideah <vasapol.rittideah@gmail.com>
2026-03-15 10:38:30 +0700

    fix(splash): correct import path for AccountNotifier

--- lib/src/features/splash/views/splash_page.dart
+++ lib/src/features/splash/views/splash_page.dart
@@ -8,7 +8,7 @@ import 'package:money_tracker/src/core/gen/assets.gen.dart';
 import 'package:money_tracker/src/core/session/session_notifier.dart';
 import 'package:money_tracker/src/core/theme/theme_provider.dart';
 import 'package:money_tracker/src/core/widgets/feedback/loading_indicator.dart';
-import 'package:money_tracker/src/features/auth/viewmodels/account/account_notifier.dart';
+import 'package:money_tracker/src/features/account/viewmodels/account/account_notifier.dart';
 
 class SplashPage extends HookConsumerWidget {
   const SplashPage({super.key});
